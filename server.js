7fa1086a0ac525975173b6784f0b6c3b04d40593
abdelmalek <abdelmalek@gmail.com>
2024-03-12 01:50:19 -0700

    fix

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 import express from 'express';
-import indexRouter from './routes/index';
+import router from './routes';
 
 const app = express();
+app.use(express.json({ limit: '100mb' }));
+app.use(router);
+app.listen(process.env.PORT || 5000);
 
-const port = process.env.PORT || 5000;
-
-app.use(express.json());
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-app.use('/', indexRouter);
-
-module.exports = app;
+export default app;
